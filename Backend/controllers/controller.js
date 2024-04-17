023937bbfd3f1d1e898b239cfbd22951920dfe6f
Arunesh <aruneshsingh827@gmail.com>
2024-04-17 09:57:03 +0530

    commit

--- Backend/controllers/controller.js
+++ Backend/controllers/controller.js
@@ -8,7 +8,7 @@ export const home = async(req, res) => {
     try {
         const csvFiles = await csvSchemaModel.find({});
 
-        return res.render('homepage', {
+        return res.render('homePage', {
             files: csvFiles,
             title: 'Home',
             style: '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">',
